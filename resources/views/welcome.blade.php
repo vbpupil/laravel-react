e99d878cf34d4d60859b0693f4218fb98928f156
Dean Haines <dhaines@workforcesoftware.com>
2022-05-14 09:42:24 +0100

    initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel React</title>
+</head>
+<body>
+<div id="hello-react"></div>
+<div id="counter"></div>
+
+<script src="{{ asset('js/manifest.js') }}"></script>
+<script src="{{ asset('js/vendor.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
